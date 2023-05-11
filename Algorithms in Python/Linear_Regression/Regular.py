72fa4aa663988fbcaf69c3f04865592b1b2438f5
Ashwin Verma <111328257+Ashwin835@users.noreply.github.com>
2023-05-10 21:05:46 -0400

    Update Regular.py

--- Algorithms in Python/Linear_Regression/Regular.py
+++ Algorithms in Python/Linear_Regression/Regular.py
@@ -1,21 +1,21 @@
-#REGULAR LINEAR REGRESSION
 import numpy as np
 class LinearRegression:
-    def __init__(self):
-        self.lr=0.01
-        self.iterations=10000
+    def __init__(self,lr=.01,iterations=10000,bias=0):
+        self.lr=lr
+        self.iterations=iterations
         self.weights=None
-        self.bias=0 
+        self.bias=bias 
         
     def fit(self,x,y):
         samples,features=x.shape
         self.weights=np.zeros(features)
         
         for i in range (0,self.iterations):
-            gradient=np.dot(np.transpose(x),(y-(np.dot(x,self.weights)+ self.bias)))
+            prediction= y-((np.dot(x,self.weights)+ self.bias))
+            gradient=(-2/samples) *(np.dot(np.transpose(x), prediction))
             self.weights=self.weights-(self.lr*gradient)
             
-            self.bias=self.bias-(self.lr*np.sum(y-(np.dot(x,self.weights))))
+            self.bias=self.bias-(self.lr*((-2/samples)*np.sum(y-(np.dot(x,self.weights)))))
     
     def predict(self,z):
-        return np.dot(z,self.weights)+ self.bias
+        return (np.dot(z,self.weights)+ self.bias)
